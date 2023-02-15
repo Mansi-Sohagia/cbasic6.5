a09a3b77578e2e208a707a0ec6d53f7ba076b7f5
TechJerryyy <msohagia@aakashinformatics.com>
2023-02-15 17:02:37 +0530

    Add project files.

---
+++ cbasic6.5/Program.cs
@@ -0,0 +1,43 @@
+﻿/*5 - Write a program and ask the user to supply a list of comma separated numbers (e.g 5, 1, 9, 2, 10). 
+    If the list is empty or includes less than 5 numbers, display "Invalid List" and ask the user to re-try; otherwise, display the 3 smallest numbers in the list.
+ */
+
+using System;
+using System.Collections.Generic;
+
+namespace cbasic6._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SolutionFive();
+        }
+        public static void SolutionFive()
+        {
+            var list = new List<int>();
+            while (true)
+            {
+                Console.WriteLine("Type a list of number, separated by commas");
+                var input = Console.ReadLine();
+
+                var array = input.Split(',');
+
+                if ((array.Length == 0) || (array.Length < 5))
+                {
+                    Console.WriteLine("Invalid list! Try again.");
+                }
+                else
+                {
+                    foreach (var number in array)
+                        list.Add(Convert.ToInt32(number));
+                    break;
+                }
+            }
+            list.Sort();
+
+            for (int i = 0; i < 3; i++)
+                Console.Write("{0} ", list[i]);
+        }
+    }
+}
